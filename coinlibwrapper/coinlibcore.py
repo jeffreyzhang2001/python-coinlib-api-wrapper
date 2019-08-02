b84eba03a6dc8651ab5dfb686db06b4cd108f2fa
Jeffrey Zhang <jeffrey346@gmail.com>
2019-08-01 23:53:12 -0400

    /coin endpoint access implemented

--- coinlibwrapper/coinlibcore.py
+++ coinlibwrapper/coinlibcore.py
@@ -1,32 +1,20 @@
 # coinlibcore.py
 import requests
-from .models import coin_data
+from .models.coin_data import CoinData
 
 class Coinlib():
     BASE_URL = "https://coinlib.io/api/v1"
-    def __init__(self, api_key): # A Coinlib API key. More info at: https://coinlib.io/apidocs.
+    def __init__(self, api_key):
         self.api_key = api_key
 
-    def get_url(self, endpoint, symbol, pref_currency="USD"):
+    # Multi-endpoint URL generator that combines base url, endpoint, api key, symbol, and pref currency
+    def get_url(self, endpoint, symbol, pref_currency):
         url = f"{self.BASE_URL}{endpoint}?key={self.api_key}&pref={pref_currency}&symbol={symbol}"
-        return(url)
-        #api_response_json = (requests.get(url)).json()
-        #return(api_response_json["price"]+f" {symbol}")
+        api_response_json = (requests.get(url)).json()
+        return api_response_json
 
-    # When called on Coinlib instance, creates Coin instance
-    def coin(self, coin_symbol):
-        # Creates an object using imported coin_data class
-        # This object must contain the following functionality:
-            # coin_instance.name
-            # coin_instance.symbol
-            # coin_instance.price
-            # coin_instance.rank
-            # coin_instance.market_cap
-            # coin_instance.total_volume_24h
-            # coin_instance.high_24h
-            # coin_instance.low_24h
-            # coin_instance.delta_1h
-            # coin_instance.delta_24h
-            # coin_instance.delta_7d
-            # coin_instance.delta_30d
-        pass
+    # Instantiate a coin_instance with coinlib_instance.coin()
+    def coin(self, coin_symbol, pref_currency="USD"):
+        api_response_json = self.get_url("/coin", coin_symbol, pref_currency)
+        coin_instance = CoinData(api_response_json)
+        return coin_instance
--- coinlibwrapper/models/coin_data.py
+++ coinlibwrapper/models/coin_data.py
@@ -1,6 +1,41 @@
-# coin_data.py
-# Models are for /coin API endpoint
+'''
+coin_data.py
+Model is for /coin API endpoint
+'''
 
 class CoinData():
-    def __init__(self, symbol):
-        pass
+    def __init__(self, api_response_json):
+        self.api_response_json = api_response_json
+
+    def name(self):
+        return self.api_response_json['name']
+
+    def price(self):
+        return self.api_response_json['price']
+
+    def rank(self):
+        return self.api_response_json['rank']
+
+    def market_cap(self):
+        return self.api_response_json['market_cap']
+
+    def total_volume_24h(self):
+        return self.api_response_json['total_volume_24h']
+
+    def high_24h(self):
+        return self.api_response_json['high_24h']
+
+    def low_24h(self):
+        return self.api_response_json['low_24h']
+
+    def delta_1h(self):
+        return self.api_response_json['delta_1h']
+
+    def delta_24h(self):
+        return self.api_response_json['delta_24h']
+
+    def delta_7d(self):
+        return self.api_response_json['delta_7d']
+
+    def delta_30d(self):
+        return self.api_response_json['delta_30d']
